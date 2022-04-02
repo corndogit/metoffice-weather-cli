58639a7de31c109b5526309680702e075470a850
Nathan <101812777+corndogit@users.noreply.github.com>
2022-04-02 21:18:29 +0100

    geocoding implemented

--- weather.py
+++ weather.py
@@ -7,23 +7,29 @@ import json
 
 load_dotenv()
 
-location = input("Please enter desired location.\t")
+location = input("Please enter desired location:\t")
 
 # connect to geocoding API
 geocode_conn = http.client.HTTPSConnection("geocode.xyz")
 geocode_params = urllib.parse.urlencode({
     'auth': os.getenv('GEOCODE_AUTH'),
     'locate': location,
-    'json': 1,
+    'json': 1
 })
 
 geocode_conn.request("GET", "/?{}".format(geocode_params))
 
 geocode_res = geocode_conn.getresponse()
-geocode_json = geocode_res.read()  # reads JSON file from request
+geocode_json = json.loads(geocode_res.read())  # reads JSON file from response
 
+geocode_values = (
+    geocode_json['standard']['city'],
+    geocode_json['standard']['countryname'],
+    geocode_json['latt'],
+    geocode_json['longt']
+)
+print(geocode_values)
 
-'''
 # connect to Weather DataHub
 datahub_conn = http.client.HTTPSConnection("rgw.5878-e94b1c46.eu-gb.apiconnect.appdomain.cloud")
 
@@ -34,15 +40,14 @@ headers = {
     }
 
 
-datahub_conn.request("GET", f"/metoffice/production/v0/forecasts/point/hourly?excludeParameterMetadata=false&includeLocationName=true&latitude={latitude}&longitude={longitude}", headers=headers)
+datahub_conn.request("GET", f"/metoffice/production/v0/forecasts/point/hourly?excludeParameterMetadata=false&includeLocationName=true&latitude={geocode_values[2]}&longitude={geocode_values[3]}", headers=headers)
 
 datahub_res = datahub_conn.getresponse()
 datahub_json = datahub_res.read()
 
 
-# data dump example
+# data dump
 with open('dump.json', 'wb') as datahub_dump:
     datahub_dump.write(datahub_json)
     print("Data written to dump.json")
     datahub_dump.close()
-'''
